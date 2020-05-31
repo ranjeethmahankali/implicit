3005e063bf978ef0c458447a123c098ceed130d2
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2020-05-31 21:06:54 +0200

    Add opencl dependency

--- implicit/implicit.cpp
+++ implicit/implicit.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#define __CL_ENABLE_EXCEPTIONS
+//#define __NO_STD_STRING
+#define  _VARIADIC_MAX 10
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#include <CL/cl.hpp>
+
 int main()
 {
+    cl::Device device = cl::Device::getDefault();
 }
