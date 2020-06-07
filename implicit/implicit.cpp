16a9eae3696554fdc4b00672cb41911b516ac382
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2020-06-07 19:30:47 +0200

    Partially working version

--- implicit/implicit.cpp
+++ implicit/implicit.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -11,6 +12,8 @@
 #define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include <CL/cl.hpp>
+
+#include "kernel_sources.h"
 
 #ifdef _DEBUG
 #define GL_CALL(fncall) {\
@@ -27,7 +30,8 @@ static GLFWwindow* s_window;
 static cl::ImageGL s_texture;
 static cl::Context s_context;
 static cl::CommandQueue s_queue;
-static uint32_t s_texId;
+static uint32_t s_pboId = 0;
+static cl::BufferGL s_pBuffer;
 
 static bool log_gl_errors(const char* function, const char* file, uint32_t line)
 {
@@ -62,7 +66,7 @@ static void clear_gl_errors()
     }
 }
 
-static void viewer_init()
+static void init_ogl()
 {
     /* Initialize the library */
     /*glfwWindowHint(GLFW_VERSION_MAJOR, 4);
@@ -124,16 +128,78 @@ static void init_ocl()
     }
 };
 
+static void init_pbo()
+{
+    // Initialize the pixel buffer object.
+    if (s_pboId)
+    {
+        GL_CALL(clReleaseMemObject(s_pBuffer()));
+        GL_CALL(glDeleteBuffersARB(1, &s_pboId));
+    }
+
+    std::vector<uint32_t> temp(WIN_W * WIN_H);
+    std::generate(temp.begin(), temp.end(), []() { return (uint32_t)std::rand(); });
+
+    GL_CALL(glGenBuffersARB(1, &s_pboId));
+    GL_CALL(glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, s_pboId));
+    GL_CALL(glBufferDataARB(GL_PIXEL_UNPACK_BUFFER_ARB, WIN_W * WIN_H * sizeof(uint32_t), temp.data(), GL_STREAM_DRAW_ARB));
+    GL_CALL(glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, 0));
+
+    try
+    {
+        cl_int err = 0;
+        s_pBuffer = cl::BufferGL(s_context, CL_MEM_WRITE_ONLY, s_pboId, &err);
+        if (err)
+        {
+            std::cerr << "OpenCL Error" << std::endl;
+            exit(1);
+        }
+    }
+    catch (cl::Error error)
+    {
+        std::cerr << "OpenCL Error" << std::endl;
+        exit(1);
+    }
+}
+
+void render()
+{
+    static const cl::Program s_program(cl_kernel_sources::noise, true);
+    static cl::make_kernel<cl::BufferGL&> s_kernel(s_program, "k_add_noise");
+    static const std::vector<cl::Memory> memBufs({ s_pBuffer });
+    static const cl::EnqueueArgs args(cl::NDRange(WIN_W, WIN_H));
+
+    try
+    {
+        /*s_queue.enqueueAcquireGLObjects(&memBufs);
+        s_queue.finish();
+        s_kernel(args, s_pBuffer);
+        s_queue.enqueueReleaseGLObjects(&memBufs);
+        s_queue.finish();*/
+    }
+    catch (cl::Error error)
+    {
+        std::cerr << "Open CL error" << std::endl;
+    }
+}
+
 int main()
 {
+    init_ogl();
     init_ocl();
-    viewer_init();
+    init_pbo();
     size_t i = 0;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(s_window))
     {
-        /* Render here */
+        render();
         glClear(GL_COLOR_BUFFER_BIT);
+        glDisable(GL_DEPTH_TEST);
+
+        glRasterPos2i(-1, -1);
+        glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, s_pboId);
+        glDrawPixels(WIN_W, WIN_H, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+        glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(s_window);
--- implicit/kernel_sources.h
+++ implicit/kernel_sources.h
@@ -1,4 +1,6 @@
-
+namespace cl_kernel_sources
+{
+	constexpr char noise[] = R"(
 uint rand(uint2 coord)
 {
   uint seed = coord.x + coord.y;
@@ -6,13 +8,14 @@ uint rand(uint2 coord)
   uint result = coord.y ^ (coord.y >> 19) ^ (t ^ (t >> 8));
   return result;
 }
-
 kernel void k_add_noise(image2d_t pBuffer)
 {
   uint2 coord = (uint2)(get_global_id(0), get_global_id(1));
   uint result = rand(coord);
-
   float val = ((float)(result % 256) / 256.0f);
   uint i = coord.x + (coord.y * get_global_size(0));
   /* pBuffer[i] = val; */
+}
+	)";
+
 }
